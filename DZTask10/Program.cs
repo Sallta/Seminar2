d654cd74a67c6cdb410eb9810710af379a6ec390
Saltanat <saltushka1989@yahoo.com>
2023-07-30 11:52:39 +0300

    Домашнее задание. Задача 10

---
+++ DZTask10/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// int firstDigit=number/100;
+// int thirdDigit=number%10;
+// int result=firstDigit*10+thirdDigit;
+// Console.WriteLine($"Результат удаления второй цифры -> {result}");
+
+
+int result=DelSecondDigit(number);
+Console.WriteLine($"результат удаления второй цифры -> {result}");
+
+int result1=DelSecondDigit(567);
+Console.WriteLine($"результат удаления второй цифры -> {result1}");
+
+int DelSecondDigit(int num)
+{
+    int firstDigit=num/100;
+    int thirdDigit=num%10;
+    return firstDigit*10+thirdDigit;
+}
